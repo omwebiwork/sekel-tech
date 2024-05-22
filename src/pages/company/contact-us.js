212024740cdfe9ad2d9c3e78c12ab4e215bb5323
Aniket Panday <pandeyaniket546@gmail.com>
2024-05-22 19:33:15 +0530

    Contact us page mobile UI DONE

--- src/pages/company/contact-us.js
+++ src/pages/company/contact-us.js
@@ -38,10 +38,16 @@ const ContactUs = () => {
         renderElement={
           <>
             <div className="max-lg:hidden lg:-mt-[200px] lg:-mr-[80px]">
-              <Image className="w-full object-cover" {...bannerDiscoverPower?.renderImage} />
+              <Image
+                className="w-full object-cover"
+                {...bannerDiscoverPower?.renderImage}
+              />
             </div>
             <div className="lg:hidden -mb-[100px]">
-              <Image className="w-full max-w-[90%]" {...bannerDiscoverPower?.renderMobileImage} />
+              <Image
+                className="w-full max-w-[90%]"
+                {...bannerDiscoverPower?.renderMobileImage}
+              />
             </div>
           </>
         }
@@ -59,34 +65,37 @@ const ContactUs = () => {
           { link: "/contact-us", label: "Contact" },
         ]}
       />
-      <section className="container">
-        <div className="grid grid-cols-3 gap-5 py-[50px] bg-white">
-          {information?.map((item, index) => {
-            return (
-              <div className="col-span-1" key={index}>
-                <Card
-                  {...item}
-                  headingSty="text-xl font-medium leading-[30px] mb-[14px] text-black-900"
-                  cardSty="bg-gray-100 px-5 py-5 rounded-2xl border-[1px] border-gray-400 h-full "
-                  cardDataSty="flex gap-6"
-                  dataDivSty="max-w-[206px]"
-                  titleIconSty="h-[77px] w-[77px] rounded-full bg-yellow-100 flex items-center justify-center mb-8 "
-                />
-              </div>
-            );
-          })}
+      <section className="py-[50px]">
+        <div className="container">
+          <div className="grid grid-cols-3 gap-5 bg-white">
+            {information?.map((item, index) => {
+              return (
+                <div className="col-span-3 lg:col-span-1" key={index}>
+                  <Card
+                    {...item}
+                    headingSty="text-xl font-medium leading-[30px] mb-[14px] text-black-900"
+                    cardSty="bg-gray-100 px-5 py-5 rounded-2xl border-[1px] border-gray-400 h-full "
+                    cardDataSty="flex flex-wrap"
+                    dataDivSty="w-[calc(100%_-_77px)] pl-5"
+                    descriptionSty="text-base font-normal leading-[140%] text-black-33"
+                    titleIconSty="h-[77px] w-[77px] rounded-full bg-yellow-100 flex items-center justify-center lg:mb-8 "
+                  />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
 
       <InfoCard
         {...seamlessIntegration}
-        sectionStyle="pt-[50px] pb-[100px] px-[42px]  bg-white"
-        containtWidth="max-w-[371px]"
-        textContainerSty="col-span-5 flex items-center"
+        sectionStyle="py-[50px] lg:pt-[50px] lg:pb-[100px] bg-white"
+        containtWidth="lg:max-w-[371px]"
+        textContainerSty="col-span-11 max-lg:mb-4 lg:col-span-5 flex items-center"
         descriptionSty="mb-8 text-base font-normal leading-[25px]"
-        imageContainerSty="max-h-[504px] w-full max-w-[624px] rounded-[20px] overflow-hidden mx-auto"
-        imageContentSty="col-span-6"
-        titleSty="text-[42px] font-medium leading-[52px] tracking-tighter text-black-33 mb-8"
+        imageContainerSty="lg:max-h-[504px] w-full lg:max-w-[624px] rounded-[20px] overflow-hidden mx-auto"
+        imageContentSty="col-span-11 lg:col-span-6"
+        titleSty="text-[42px] max-lg:text-center font-medium leading-[140%] tracking-tighter text-black-33 mb-8"
         leadTextSty="mb-8"
         renderElement={
           <Button
@@ -106,17 +115,17 @@ const ContactUs = () => {
 
       <InfoCard
         {...frequentlyAsked?.sectionData}
-        sectionStyle="py-[100px] bg-blue-900"
+        sectionStyle="py-[50px] md:py-[65px] lg:py-[100px] bg-blue-900"
         containerSty="container "
         containtWidth=""
-        titleSty="text-white text-[42px] font-medium mb-8 leading-[62px] tracking-tighter"
-        descriptionSty="text-white max-w-[542px] mb-8 mr-auto text-base font-normal leading-[25px]"
-        imageContainerSty=" w-full max-w-[497px] mx-auto"
-        textContainerSty=" col-span-6 order-2 flex items-center"
-        imageContentSty=" col-span-5 order-1 flex items-center"
+        titleSty="text-white text-[42px] max-lg:text-center font-medium mb-8 leading-[140%] tracking-tighter"
+        descriptionSty="text-white lg:max-w-[542px] mb-8 mr-auto text-base font-normal leading-[25px]"
+        imageContainerSty="w-full lg:max-w-[497px] mx-auto"
+        textContainerSty="max-lg:mt-5 col-span-11 lg:col-span-6 order-2 lg:flex items-center"
+        imageContentSty="col-span-11 lg:col-span-5 order-1 flex items-center"
         renderElement={
           <>
-            <div className=" grid  max-w-xl mx-auto mt-8">
+            <div className=" grid lg:max-w-xl lg:mx-auto mt-5 lg:mt-8">
               {frequentlyAsked?.faqList?.map((item, index) => {
                 return (
                   <div className="py-2">
